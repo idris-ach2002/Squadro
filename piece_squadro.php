441d03f735eb538ce52d61092dc30977fdcb9788
Idris Achabou <idris.achabou@univ-lehavre.fr>
2025-01-17 14:59:44 +0100

    initiaisation de la classe PieceSquadro

---
+++ piece_squadro.php
@@ -0,0 +1,17 @@
+class PieceSquadro {
+    // type de case 
+    public const NOIR = 1;
+    public const VIDE = -1;
+    public const BLANC = 0;
+    public const NEUTRE = -2;
+    public const NORD = 0;
+    public const EST = 1;
+    public const SUD = 2;
+    public const OUEST = 3;
+    
+
+    //variable d'instances
+    protected int couleur;
+    protected int direction;
+
+}
